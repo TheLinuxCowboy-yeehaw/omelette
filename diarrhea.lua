1ded4d3101d05e03e1955528362edab6d29d0ef4
Avenze <contact.avenze@gmail.com>
2020-09-03 14:17:24 +0200

    diarrhea
    diarrhea

---
+++ diarrhea.lua
@@ -0,0 +1,10 @@
+local diarrhea = "diarrhea"
+if typeof(diarrhea) == "string" then
+if diarrhea == "diarrhea" then
+print("diarrhea")
+print("loads of diarrhea")
+end
+end
+
+-- 5/10 on yelp, not much flavour, could use a bit of diarrhea.
+-- recommended with diarrhea as topping, just squat over it and diarrhea all over it.
